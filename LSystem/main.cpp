d192f45a4baf18f1afa3e13db1dd1ad2fedb9d2c
cjtorralba <cjtorralba@gmail.com>
2023-06-12 19:19:47 -0700

    Added tree function

--- LSystem/main.cpp
+++ LSystem/main.cpp
@@ -29,4 +29,3 @@ int main(int argc, char **argv) {
 
     exit(EXIT_SUCCESS);
 }
-
--- Recursive/main.cpp
+++ Recursive/main.cpp
@@ -6,6 +6,54 @@
 using namespace std;
 
 
+
+void draw_tree(double *p0, double *p1, int curr_iteration, int depth, double f) {
+
+    if(curr_iteration > depth)
+        return;
+
+    double p2[] = {0.0, 0.0};
+    double p3[] = {0.0, 0.0};
+
+    double x2 = p1[0] - (p1[1] - p0[1]);
+    double y2 = p1[1] + (p1[0] - p0[0]);
+
+    double x3 = p0[0] - (p1[1] - p0[1]);
+    double y3 = p0[1] + (p1[0] - p0[0]);
+
+    double xm = x3 + f * (x2-x3);
+    double ym = y3 + f * (y2 - y3);
+
+    double g = sqrt(f * (1-f));
+    double xq = xm - g * (y2-y3);
+    double yq = ym + g * (x2 - x3);
+
+    G_rgb(.4 - ((double) curr_iteration / 50), .2 + ((double) curr_iteration / 10), 0.1);
+
+    G_line(p0[0], p0[1], p1[0], p1[1]);
+    G_line(p1[0], p1[1], x2, y2);
+    G_line(x2, y2, x3, y3);
+    G_line(p0[0], p0[1], x3, y3);
+
+    p2[0] = x2;
+    p2[1] = y2;
+
+    p3[0] = x3;
+    p3[1] = y3;
+
+    double q[] = {xq, yq};
+
+    draw_tree(q, p2, curr_iteration + 1, depth, f);
+    draw_tree(p3, q, curr_iteration + 1, depth, f);
+
+
+}
+
+
+
+
+
+
 int main(int argc, char **argv) {
 
 
